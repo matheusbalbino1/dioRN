a3d02d8b98da2bf351da951fb17ee8c282bfc141
matheusbalbino1 <matheusbalbinodeoliveira3@gmail.com>
2022-05-23 20:10:44 -0300

    att

--- src/App.js
+++ src/App.js
@@ -18,6 +18,24 @@ function App() {
         }
     }
 
+    let teste = [15,19,23,27,29,32,35,37,40,42,45,48,50,53,54,58,62]
+    let coresDio = []
+    let key = 0
+    let count = 0 
+
+
+    for (let i =0;i<78;i++){
+        if (i == 14 || i == 15 || i == 19 || i == 23 || i == 27 || i == 29 || i ==  32 || i == 35 || i == 37 || i ==  40 || i == 42 || i == 45 || i == 48 || i == 50 || i == 53 || i == 54 || i == 58 || i == 62){
+            coresDio.push("#98FB98")
+        }else{
+            coresDio.push("#228B22")
+        }
+    }
+
+
+    console.log(coresDio)
+
+
     return (
         <SafeAreaView style={style.container}>
             <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
@@ -27,11 +45,20 @@ function App() {
                 <Text accessibilityLabel="Apelido matheusbalbino1" style={[style.defaultText, style.nickname]}>matheusbalbino1</Text>
                 <Text accessibilityLabel="Descrição html5, css3, bootstrap, javascript" style={[style.defaultText, style.description]}>HTML5 | CSS3 | BOOTSTRAP | JAVASCRIPT Aprofundando em Javascript!</Text>
             </View>
-            <Pressable onPress={handlePressGoToGithub}>
+            <View style={style.formatContainer}>
+                {
+                coresDio.map((parametro)=>{
+                    return <View style={[style.formatDio,{backgroundColor:parametro}]}></View>
+                })}
+            </View>
+            
+            <Pressable onPress={handlePressGoToGithub} style={style.content}>
                 <View style={style.button}>
                     <Text style={[style.defaultText, style.textButton]}>Open in Github</Text>
                 </View>
+                
             </Pressable>
+            
         </SafeAreaView>
     )
 }
@@ -39,14 +66,32 @@ function App() {
 export default App;
 
 const style = StyleSheet.create({
+    formatContainer:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginTop:20,
+        marginBottom:20,
+        flexWrap:"wrap",
+        width:330
+    },
+    formatDio:{
+        width:20,
+        height:20,
+        backgroundColor:"green",
+        marginLeft:5,
+        marginBottom:5,
+        borderRadius:5
+    },
     container: {
         backgroundColor: colorGithub,
         flex: 1, // EXPANDIR A TELA INTEIRA
         justifyContent: "center",
+        alignItems:"center"
     },
     content: {
         alignItems: "center",
-        padding: 20,
+        padding: 15,
+        justifyContent:"center"
     },
     avatar: {
         height: 200,
@@ -75,7 +120,7 @@ const style = StyleSheet.create({
         backgroundColor: colorDarkFontGithub,
         borderRadius: 10,
         padding: 20,
-        marginTop: 20
+        width:150,
     },
     textButton: {
         fontWeight: "bold",
